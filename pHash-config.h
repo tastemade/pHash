418f7f58e2be0e5d1ec1f5bbc1ef02ae560f76ca
Mike <mike@tastemade.com>
2017-06-22 13:36:37 -0500

    added .gitignore

--- pHash-config.h
+++ pHash-config.h
@@ -1,6 +1,6 @@
 /* pHash-config.h.  Generated from pHash-config.h.in by configure.  */
 /* configure with audio hash */
-#define HAVE_AUDIO_HASH 1
+/* #undef HAVE_AUDIO_HASH */
 
 /* configure with video hash */
 #define HAVE_VIDEO_HASH 1
